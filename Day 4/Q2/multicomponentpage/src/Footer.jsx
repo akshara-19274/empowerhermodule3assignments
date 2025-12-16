feed56df8dd7f73214ebac947ea5d1b599d8c7b9
akshara-19274 <gakshara19274@gmail.com>
2025-12-16 21:03:43 +0530

    1

---
+++ Day 4/Q2/multicomponentpage/src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div style={{ fontSize: "14px", color: "gray", marginTop: "30px", textAlign: "center" }}>
+      This is Footer
+    </div>
+  );
+}
+
+export default Footer;
